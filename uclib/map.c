996c6ed61fc6a55a95b681567a1efee15ba52f5a
jim <xianjimli@hotmail.com>
2017-05-13 18:40:46 +0800

    add map doc

--- uclib/map.c
+++ uclib/map.c
@@ -1,3 +1,11 @@
+/* 
+ * 功能说明：
+ *     1.map_t类的实现。
+ *
+ * 修改历史：
+ *     1.2017-5-6 李先静 创建。
+ */
+
 #include "map.h"
 #include "value_helper.h"
 
@@ -5,12 +13,12 @@ struct _map_t {
     array_t* array;
 };
 
-static key_value_t* key_value_create(const char* key, value_t value, bool_t deep_copy) {
+static key_value_t* key_value_create(const char* key, value_t value) {
     key_value_t* kv = calloc(1, sizeof(key_value_t));
     return_value_if_fail(kv != NULL, NULL);
 
     kv->key = str_create(key, strlen(key), 0);
-    value_copy(&kv->value, &value, deep_copy);
+    value_copy(&kv->value, &value, FALSE);
     
     return kv;
 }
@@ -28,13 +36,13 @@ static bool_t key_value_destroy(key_value_t* kv) {
 }
 
 static int compare_kv(const char* a, value_t* v) {
-    key_value_t* b = (key_value_t*)value_pointer(v);
+    key_value_t* b = (key_value_t*)value_pointer(*v);
     return strcmp(a, b->key->str);
 }
 
-map_t* map_create(bool_t deep_copy) {
+map_t* map_create() {
     map_t* map = NULL;
-    array_t* array = array_create(deep_copy);
+    array_t* array = array_create();
     return_value_if_fail(array != NULL, NULL);
     
     map = (map_t*)calloc(1, sizeof(map_t));
@@ -47,17 +55,17 @@ map_t* map_create(bool_t deep_copy) {
     return map;
 }
 
-value_t*  map_get(map_t* map, const char* key) {
+value_t  map_get(map_t* map, const char* key) {
     int pos = 0;
     key_value_t* kv = NULL;
-    return_value_if_fail(map != NULL && map->array != NULL && key != NULL, NULL);
+    return_value_if_fail(map != NULL && map->array != NULL && key != NULL, value_null);
     
     pos = array_find(map->array, (compare_t)compare_kv, (void*)key);
-    return_value_if_fail(pos >= 0, NULL);
+    return_value_if_fail(pos >= 0, value_null);
 
     kv = value_pointer(array_get(map->array, pos));
 
-    return &(kv->value);
+    return kv->value;
 }
 
 bool_t    map_set(map_t* map, const char* key, value_t data) {
@@ -66,11 +74,11 @@ bool_t    map_set(map_t* map, const char* key, value_t data) {
     
     pos = array_find(map->array, (compare_t)compare_kv, (void*)key);
     if(pos < 0) {
-        key_value_t* kv = key_value_create(key, data, map->array->deep_copy);
+        key_value_t* kv = key_value_create(key, data);
         return array_append(map->array, value_from_pointer(kv, (destroy_t)key_value_destroy));  
     }else{
         key_value_t* kv = value_pointer(array_get(map->array, pos));
-        value_copy(&kv->value, &data, map->array->deep_copy);
+        value_copy(&kv->value, &data, FALSE);
 
         return TRUE;
     }
--- uclib/map.h
+++ uclib/map.h
@@ -1,3 +1,11 @@
+/* 
+ * 功能说明：
+ *     1.map_t类的声明。
+ *
+ * 修改历史：
+ *     1.2017-5-6 李先静 创建。
+ */
+ 
 #include "uclib/str.h"
 #include "uclib/array.h"
 
@@ -6,6 +14,10 @@
 
 BEGIN_C_DECLS
 
+/**
+ * @class map_t
+ * 存放key/value映射的容器，方便通过key查找value。
+ */
 struct _map_t;
 typedef struct _map_t map_t;
 
@@ -14,15 +26,81 @@ typedef struct _key_value_t {
 	value_t value;
 }key_value_t;
 
-map_t* map_create(bool_t deep_copy);
+/**
+ * @method map_create
+ * 创建map。
+ *
+ * @return {map_t*} map对象。
+ */
+map_t* map_create();
 
+/**
+ * @method map_size
+ * 获取map中元素的个数。
+ * @param {map_t*} map map对象。
+ *
+ * @return {uint32_t} 元素的个数。
+ */
 uint32_t  map_size(map_t* map);
-value_t*  map_get(map_t* map, const char* key);
+
+/**
+ * @method map_get
+ * 获取指定key的value。
+ * @param {map_t*}      map map对象。
+ * @param {const char*} key 键名。
+ *
+ * @return {value_t} 值。
+ */
+value_t   map_get(map_t* map, const char* key);
+
+/**
+ * @method map_set
+ * 设置指定key的value。
+ * @param {map_t*}      map map对象。
+ * @param {const char*} key 键名。
+ * @param {value_t}     value 值。
+ *
+ * @return {bool_t} 成功返回TRUE，失败返回FALSE。
+ */
 bool_t    map_set(map_t* map, const char* key, value_t data);
+
+/**
+ * @method map_delete
+ * 删除指定key。
+ * @param {map_t*}      map map对象。
+ * @param {const char*} key 键名。
+ *
+ * @return {bool_t} 成功返回TRUE，失败返回FALSE。
+ */
 bool_t    map_delete(map_t* map, const char* key);
+
+/**
+ * @method map_foreach
+ * 遍历所有的key/value。
+ * @param {map_t*}     map   map对象。
+ * @param {visit_t}    visit 遍历函数。
+ * @param {void*}      ctx   遍历函数的上下文。
+ *
+ * @return {bool_t} 成功返回TRUE，失败返回FALSE。
+ */
 bool_t    map_foreach(map_t* map, visit_t visit, void* ctx);
+
+/**
+ * @method map_clear
+ * 清除所有元素。
+ * @param {map_t*}     map   map对象。
+ *
+ * @return {bool_t} 成功返回TRUE，失败返回FALSE。
+ */
 bool_t    map_clear(map_t* map);
 
+/**
+ * @method map_destroy
+ * 销毁map对象。
+ * @param {map_t*}     map   map对象。
+ *
+ * @return {void} 无。
+ */
 void map_destroy(map_t* map);
 
 END_C_DECLS

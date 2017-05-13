8f89a0101ce5c599ce21606df72711922e245152
jim <xianjimli@hotmail.com>
2017-05-13 18:15:17 +0800

    add event doc

--- uclib/event.c
+++ uclib/event.c
@@ -1,23 +1,30 @@
+/* 
+ * 功能说明：
+ *     1.event_t类的实现。
+ *
+ * 修改历史：
+ *     1.2017-5-6 李先静 创建。
+ */
 #include "event.h"
 #include "value_helper.h"
 
-event_t* event_create(const char* name) {
+event_t* event_create(const char* type) {
     event_t* event = NULL;
-    return_value_if_fail(name != NULL, NULL);
+    return_value_if_fail(type != NULL, NULL);
 
     event = (event_t*)calloc(1, sizeof(event_t));
     return_value_if_fail(event != NULL, NULL);
 
     do {
-        event->name = str_create(name, strlen(name), 0);
-        break_if_fail(event->name != NULL);
+        event->type = str_create(type, strlen(type), 0);
+        break_if_fail(event->type != NULL);
         event->params = map_create(FALSE); 
         event->ref = 1;
     }while(0);
 
-    if(event->name == NULL || event->params == NULL) {
-        if(event->name != NULL) {
-            str_unref(event->name);
+    if(event->type == NULL || event->params == NULL) {
+        if(event->type != NULL) {
+            str_unref(event->type);
         }
         if(event->params != NULL) {
             map_destroy(event->params);
@@ -29,10 +36,10 @@ event_t* event_create(const char* name) {
     return event;
 }
 
-bool_t   event_set_name(event_t* event, const char* name) {
-    return_value_if_fail(event != NULL && name != NULL, FALSE);
+bool_t   event_set_type(event_t* event, const char* type) {
+    return_value_if_fail(event != NULL && type != NULL, FALSE);
 
-    return str_set(event->name, name, strlen(name));
+    return str_set(event->type, type, strlen(type));
 }
 
 bool_t   event_set_param(event_t* event, const char* name, const char* value) {
@@ -48,9 +55,9 @@ const char* event_get_param(event_t* event, const char* name) {
 }
 
 static void event_destroy(event_t* event) {
-    return_if_fail(event != NULL && event->name != NULL && event->params != NULL);
+    return_if_fail(event != NULL && event->type != NULL && event->params != NULL);
 
-    str_unref(event->name);
+    str_unref(event->type);
     map_destroy(event->params);
     memset(event, 0x00, sizeof(event_t));
     free(event);
@@ -59,17 +66,22 @@ static void event_destroy(event_t* event) {
 }
 
 static bool_t on_param(void* ctx, value_t* v) {
-    key_value_t* kv = (key_value_t*)value_pointer(v);
-    printf("   %s = %s\n", kv->key->str, value_str(&kv->value)->str);
-    (void)ctx;
+    str_t* str = (str_t*)ctx;
+    key_value_t* kv = (key_value_t*)value_pointer(*v);
+    str_append(str, "", -1);
+    str_append(str, kv->key->str, -1);
+    str_append(str, " = ", -1);
+    str_append(str, value_str(kv->value)->str, -1);
+    str_append(str, ";", -1);
 
     return FALSE;
 }
 
-void event_dump(event_t* event) {
-    printf("%s {\n", event->name->str);
-    map_foreach(event->params, (visit_t)on_param, NULL);
-    printf("}\n");
+void event_dump(event_t* event, str_t* str) {
+    str_append(str, event->type->str, -1);
+    str_append(str, " {", -1);
+    map_foreach(event->params, (visit_t)on_param, str);
+    str_append(str, "}", -1);
 }
 
 event_t* event_ref(event_t* event) {
@@ -84,6 +96,7 @@ event_t* event_unref(event_t* event) {
     event->ref--;
     if(event->ref == 0) {
         event_destroy(event);
+        return NULL;
     }else{
         return event;
     }
--- uclib/event.h
+++ uclib/event.h
@@ -1,3 +1,10 @@
+/* 
+ * 功能说明：
+ *     1.emitter_t类的声明。
+ *
+ * 修改历史：
+ *     1.2017-5-6 李先静 创建。
+ */
 
 #include "uclib/str.h"
 #include "uclib/map.h"
@@ -7,22 +14,80 @@
 
 BEGIN_C_DECLS
 
+/**
+ * @class event_t 
+ * 事件。
+ */
 typedef struct _event_t {
     uint32_t ref;
-    str_t* name;
+    /**
+     * @property {str_t*} type
+     * 事件类型。
+     */
+    str_t* type;
     map_t* params;
 }event_t;
 
-event_t* event_create(const char* name);
-bool_t   event_set_name(event_t* event, const char* name);
+/**
+ * @method event_create
+ * 创建事件对象。
+ *
+ * @return {event_t*} 事件对象。
+ */
+event_t* event_create(const char* type);
+
+/**
+ * @method event_set_type
+ * 设置事件的类型。
+ * @param {event_t*}    event 事件对象。
+ * @param {const char*} type 事件类型。
+ *
+ * @return {bool_t} 成功返回TRUE，失败返回FALSE。
+ */
+bool_t   event_set_type(event_t* event, const char* type);
+
+/**
+ * @method event_set_param
+ * 设置事件的参数。
+ * @param {event_t*}    event 事件对象。
+ * @param {const char*} name  参数的名称。
+ * @param {const char*} value 参数的值。
+ *
+ * @return {bool_t} 成功返回TRUE，失败返回FALSE。
+ */
 bool_t   event_set_param(event_t* event, const char* name, const char* value);
+
+/**
+ * @method event_get_param
+ * 获取事件的参数。
+ * @param {event_t*}    event 事件对象。
+ * @param {const char*} name  参数的名称。
+ *
+ * @return {const char*} 参数的值。
+ */
 const char* event_get_param(event_t* event, const char* name);
 
+/**
+ * @method event_ref
+ * 增加引用计数。
+ * @param {event_t*} event 事件对象。
+ *
+ * @return {event_t*} 事件对象本身。
+ */
 event_t* event_ref(event_t* event);
+
+/**
+ * @method event_unref
+ * 减少引用计数。
+ * @param {event_t*} event 事件对象。
+ *
+ * @return {event_t*} 引用计数仍然大于0返回event对象本身，否则返回NULL。
+ */
 event_t* event_unref(event_t* event);
 
-void event_dump(event_t* event);
+void event_dump(event_t* event, str_t* str);
 
 END_C_DECLS
 
 #endif
+
